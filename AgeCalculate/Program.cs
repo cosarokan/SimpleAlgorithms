09165018dc6f7f426b9bd2a4d0073440d85dde62
Okan Coşar <82242098+cosarokan@users.noreply.github.com>
2022-04-10 01:10:18 +0300

    Add files via upload

---
+++ AgeCalculate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeCalculate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Your date of birth: ");
+            DateTime dateTime = Convert.ToDateTime(Console.ReadLine());
+            
+            Console.WriteLine("Your Age: " + AgeCalculate(dateTime));
+
+            Console.ReadKey();
+        }
+        public static int AgeCalculate(DateTime date)
+        {
+            int result = 0;
+
+            DateTime dateTime = DateTime.Now;
+
+            result = dateTime.Year - date.Year;
+
+            return result;
+        }
+    }
+}
