82db11ff4db18dc6ffc1db39dc0abdfe81cc00bf
Okan Coşar <okancosar1@hotmail.com>
2022-04-09 14:50:52 +0300

    Fibonacci numbers

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -10,6 +10,27 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
+            List<int> ar = Fibonacci(10);
+
+            foreach (int item in ar)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+        public static List<int> Fibonacci(int count)
+        {
+            List<int> ar = new List<int> { 0, 1 };
+
+            for (int i = 2; i < count; i++)
+            {
+                int twoPrevious = ar[i - 2];
+                int onePrevious = ar[i - 1];
+
+                ar.Add(twoPrevious + onePrevious);
+            }
+            return ar;
         }
     }
 }
