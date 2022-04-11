5a2124e1238e483d0f774bd2a0a91c9312228d9f
Okan Coşar <82242098+cosarokan@users.noreply.github.com>
2022-04-11 12:45:03 +0300

    Add files via upload

---
+++ HowManyDaysILived/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowManyDaysILived
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the your date of birth: ");
+            DateTime date = Convert.ToDateTime(Console.ReadLine());
+
+            Console.WriteLine("You lived " + HowManyDaysILived(date) + " days");
+
+            Console.ReadKey();
+        }
+        static int HowManyDaysILived(DateTime dateTime)
+        {
+            DateTime date = DateTime.Now;
+            TimeSpan result = (date - dateTime);
+
+            int totalDays = Convert.ToInt32(result.TotalDays);
+
+
+            return totalDays;
+        }
+    }
+}
