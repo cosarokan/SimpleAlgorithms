513c1980797449a8bff11f116417fe65d66578cd
Okan Coşar <82242098+cosarokan@users.noreply.github.com>
2022-04-12 14:25:42 +0300

    Add files via upload

---
+++ ArmstrongNumber/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArmstrongNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in FindTheArmstrongNumber(1000))
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+        static bool IsTheArmstrongNumber(int value)
+        {
+            // it finds whether the entered number is armstrong number
+            string stringValue = value.ToString();
+            int[] intArray = stringValue.ToArray().Select(x => Convert.ToInt32(x.ToString())).ToArray();
+
+            double total = 0;
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                double pow = Math.Pow(intArray[i], 3);
+                total += pow;
+            }
+            return Convert.ToInt32(total) == value;
+        }
+
+        static List<int> FindTheArmstrongNumber(int value)
+        {
+            // it returns all armstrong numbers.
+            List<int> ar = new List<int>();
+
+            for (int i = 1; i < value; i++)
+            {
+                if (IsTheArmstrongNumber(i) == true)
+                {
+                    ar.Add(i);
+                }
+            }
+            return ar;
+        }
+
+    }
+}
