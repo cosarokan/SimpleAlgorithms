641c489c1bb304d4a4dcd6c2889858451ba2e1a1
Okan Coşar <82242098+cosarokan@users.noreply.github.com>
2022-04-13 14:20:39 +0300

    Add files via upload

---
+++ FindTheChar/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTheChar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the text: ");
+            string text = Console.ReadLine();
+
+            Console.Write("Enter the char: ");
+            char x = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine(AreThereAnyCharEnteredInTheText(text, x));
+
+            Console.ReadKey();
+        }
+        static bool AreThereAnyCharEnteredInTheText(string text, char x)
+        {
+            bool result = false;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == x)
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+    }
+}
