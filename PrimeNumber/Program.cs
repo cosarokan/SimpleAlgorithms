e2afb3a5afa2e6058c6e522feb25c5c44cda61dc
Okan Coşar <82242098+cosarokan@users.noreply.github.com>
2022-04-15 00:24:53 +0300

    Add files via upload

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IsPrimeNumber(11);
+
+            Console.ReadKey();
+        }
+        static void IsPrimeNumber(int x)
+        {
+            bool isPrimeNumber = false;
+
+            for (int i = 2; i < x; i++)
+            {
+                if (x % i == 0)
+                {
+                    isPrimeNumber = true;
+                    break;
+                }
+            }
+            if (isPrimeNumber == false)
+            {
+                Console.WriteLine("It is a prime number.");
+            }
+            else
+            {
+                Console.WriteLine("It is not a prime number.");
+            }
+        }
+    }
+}
